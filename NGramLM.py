b0a6326812d381a537df07ff316c02baf74651dc
pankaj <jhawar.pankaj1990@gmail.com>
2019-08-30 20:53:11 -0500

    Create an ngram model and calculate probabilities of sentences

---
+++ NGramLM.py
@@ -0,0 +1,56 @@
+class NGramLM:
+
+    def __init__(self, n):
+        self.n = n
+        self.ngram_counts = {}
+        self.context_counts = {}
+        self.vocabulary = {"<s>", "</s>"}
+
+    def update(self, text):
+
+        # Pad the input list with <s> and </s>
+        [text.insert(i, "<s>") for i in range(self.n-1)]
+        text.insert(len(text), "</s>")
+
+        # Count the no of ngrams
+        for i in range(len(text) - self.n + 1):
+            ngram_list = []
+            for j in range(i, i + self.n):
+                ngram_list.append(text[j])
+            ngram_tuple = tuple(ngram_list)
+            if ngram_tuple in self.ngram_counts:
+                count = self.ngram_counts.get(ngram_tuple)
+                self.ngram_counts[ngram_tuple] = count + 1
+            else:
+                self.ngram_counts[ngram_tuple] = 1
+
+        # Count the no of contexts
+        for i in range(len(text) - self.n + 2):
+            context_list = []
+            for j in range(i, i + self.n - 1):
+                context_list.append(text[j])
+            context_tuple = tuple(context_list)
+            if context_tuple in self.context_counts:
+                count = self.context_counts.get(context_tuple)
+                self.context_counts[context_tuple] = count + 1
+            else:
+                self.context_counts[context_tuple] = 1
+
+        # Update the vocabulary
+        {self.vocabulary.add(word) for word in text}
+
+    def create_ngramlm(self, corpus_path):
+        file = open(corpus_path)
+        for line in file:
+            self.update(line.split())
+
+    def word_prob(self, word, context):
+        prob = 1/len(self.vocabulary)
+        ngram_tuple = tuple(context) + (word,)
+        ngram_count = self.ngram_counts.get(ngram_tuple)
+        context_count = self.context_counts.get(tuple(context))
+        if ngram_count is None:
+            ngram_count = 0
+        if context_count is not None:
+            prob = ngram_count/context_count
+        return prob
--- ngram.py
+++ ngram.py
@@ -1,3 +1,7 @@
+from NGramLM import NGramLM
+import math
+
+
 def get_ngrams(n, text):
 
     # Adding begin and end word
@@ -9,8 +13,44 @@ def get_ngrams(n, text):
         yield text[i], tuple(text[i-n+1: i])
 
 
+def text_prob(model, text):
+    prob = 0
+
+    # Pad the input list with <s> and </s>
+    [text.insert(i, "<s>") for i in range(model.n - 1)]
+    text.insert(len(text), "</s>")
+
+    # Calculate all ngram probabilities
+    for i in range(model.n - 1, len(text)):
+        val = model.word_prob(text[i], [text[context] for context in range(i - model.n + 1, i)])
+        prob = prob + math.log(val + 1) if val == 0 else prob + math.log(val)
+
+    return prob
+
+
 if __name__ == "__main__":
-    n = 3
-    text = ["ab", "cd", "ed", "df", "df"]
-    for (a, b) in get_ngrams(n, text):
-        print("Word:" + a + "Context: " + str(b))
+    n = 4
+    # text = ["ab", "cd", "ed", "df", "df"]
+    # for (a, b) in get_ngrams(n, text):
+    #     print("Word:" + a + ", Context: " + str(b))
+
+    model = NGramLM(n)
+    model.create_ngramlm("../../../files/warpeace.txt")
+
+    # Debug prints
+    # file_ngram = open("../ngram-counts.log", "w")
+    # file_ngram.write(str(model.ngram_counts))
+    # file_ngram.close()
+    #
+    # file_context = open("../context-counts.log", "w")
+    # file_context.write(str(model.context_counts))
+    # file_context.close()
+
+    # print(model.ngram_counts)
+    # print(model.context_counts)
+    # print(model.vocabulary)
+    # print(model.word_prob("than", ["out", "of", "malice"]))
+    # obj.update(["I", "love", "Computer", "Science", "Computer", "Science"])
+
+    print(text_prob(model, list("God has given it to me, let him who touches it beware!".split(" "))))
+    print(text_prob(model, list("Where is the prince, my Dauphin?".split(" "))))
